95fb413c245874364903fad4304d8ea83a0354a8
connectmilind21 <connectmilind21@gmail.com>
2025-07-25 12:49:42 -0700

    Add files via upload

---
+++ 1768. Merge Strings Alternately_Easy.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        len_word1 = len(word1)
+        len_word2 = len(word2)
+        print(word1)
+        print(word2)
+
+        result = []
+        i = 0 
+        while i < len_word1 or i < len_word2:
+            if i < len_word1:
+                result.append(word1[i])
+            if i < len_word2:
+                result.append(word2[i])
+            i+=1
+        return ''.join(result)
